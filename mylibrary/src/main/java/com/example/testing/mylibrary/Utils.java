68f71833fb5981cbce0f0bd2766db501cae7d73e
ShuxinHuang <com.shuxin.huang@gmail.com>
2017-06-20 11:26:44 +0800

    哈哈

---
+++ mylibrary/src/main/java/com/example/testing/mylibrary/Utils.java
@@ -0,0 +1,12 @@
+package com.example.testing.mylibrary;
+
+/**
+ * Created by Administrator on 2017/6/20 0020.
+ */
+
+public class Utils {
+
+    public static String getVersion(){
+        return "1.0.0";
+    }
+}
